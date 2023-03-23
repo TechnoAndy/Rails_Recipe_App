cf745cddaf85ca90104f9ddba79e1d0dea3de3fd
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2023-03-23 11:34:12 +0200

    Fix linter errors

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Food, type: :model do
   before(:each) do
     @user = User.create(name: 'User', email: '456@gmail.com', password: '123456', password_confirmation: '123456')
-    @food = Food.create(name: 'Food', measurement_unit: 'kg', price: 10 )
+    @food = Food.create(name: 'Food', measurement_unit: 'kg', price: 10)
   end
 
   describe 'validations' do
